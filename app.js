2b0f3042134c943336d8d32642ca996e562eef02
Sum Guy <gjserq@gmail.com>
2019-02-01 11:00:29 -0700

    added ReadMe

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCPXKI0iPhjsRW1lbxJOgkOo6NNmiXp-3s",
+    authDomain: "digital-footprint-1999d.firebaseapp.com",
+    databaseURL: "https://digital-footprint-1999d.firebaseio.com",
+    projectId: "digital-footprint-1999d",
+    storageBucket: "",
+    messagingSenderId: "820087665716"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
